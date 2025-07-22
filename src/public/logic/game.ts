10a92a71751b4a5c1fcf0cc7e9e7d90359cc8d77
Chase Odom <chasemichaelodom@gmail.com>
2025-07-21 22:27:01 -0600

    Adding the canvas

--- src/public/logic/game.ts
+++ src/public/logic/game.ts
@@ -2,10 +2,24 @@ import { Tuple } from "./tuple.js"; // ts extension gone
 import { Label } from "../visuals/label.js"; // ts extension gone
 
 export class Game {
+  canvas: HTMLCanvasElement | null = null;
+  ctx: CanvasRenderingContext2D | null = null;
   tuples: Array<Tuple> = [];
   labels: Array<Label> = [];
 
-  constructor() {}
+  constructor() {
+    this.canvas = document.getElementById("VGCLCanvas") as HTMLCanvasElement;
+    this.ctx = this.canvas.getContext("2d") as CanvasRenderingContext2D;
+    this.ctx.fillStyle = "red";
+    this.ctx.fillRect(0, 0, 150, 75);
+    const img = new Image();
+
+    img.addEventListener("load", () => {
+      this.ctx?.drawImage(img, 0, 0);
+    });
+
+    img.src = "visuals/images/bg.png";
+  }
 
   play() {}
 
