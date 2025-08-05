c8aec6d01c5726a37917c0cb11a23a640fcc67d9
Chase Odom <chasemichaelodom@gmail.com>
2025-08-04 21:16:35 -0600

    Using prism-code-editor instead

--- src/public/index.ts
+++ src/public/index.ts
@@ -1,4 +1,33 @@
 import { Blackjack } from "./demos/blackjack.js"; // ts extension gone
+import {
+  minimalEditor,
+  basicEditor,
+  readonlyEditor,
+} from "prism-code-editor/setups";
+// Importing Prism grammars
+import "prism-code-editor/prism/languages/markup";
 
 const game = new Blackjack();
 game.play();
+
+const codeEditorText = document.getElementById("code-editor");
+if (codeEditorText) {
+  codeEditorText.textContent = `<head>
+  <link
+    rel="stylesheet"
+    type="text/css"
+    href="stylesheets/prism.css"
+    rel="stylesheet"
+  />
+</head>
+...
+
+<pre
+  onPaste="setTimeout(function() {onPaste();}, 0)"
+  id="editable"
+  contenteditable
+>
+  <code id="yaml" class="language-yaml"></code>
+</pre>
+<script src="javascript/prism.js"></script>`;
+}
